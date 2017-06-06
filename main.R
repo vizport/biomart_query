46980d0aaaab3417e936bf72481c209ed96484b1
jotasolano <jsolano91@gmail.com>
2017-06-06 18:46:51 -0400

    fixed datafields, added family_description

--- main.R
+++ main.R
@@ -12,13 +12,13 @@ library(jsonlite)
 # querying the data
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
 all.transcripts <- getBM(attributes=c('ensembl_gene_id','ensembl_peptide_id','external_transcript_name',
-                                      'description'), mart = ensembl)
+                                      'description', 'family_description'), mart = ensembl)
 
 # getting only entries with a valid peptide id
-all.peptides <- all.transcripts %>% dplyr::select(-ensembl_gene_id) %>% dplyr::filter(ensembl_peptide_id != '')
+all.peptides <- all.transcripts %>% dplyr::select(-ensembl_gene_id, -description) %>% dplyr::filter(ensembl_peptide_id != '')
 
 # changing header names for json creation
-colnames(all.peptides) <- c('protein_id', 'protein', 'description')
+colnames(all.peptides) <- c('protein_id', 'protein', 'family_description')
 
 json <- toJSON(all.peptides)
 cat(json)
