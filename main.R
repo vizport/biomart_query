914bda499cbc0bd3836ccc10491c86fb8b36872f
jotasolano <jsolano91@gmail.com>
2017-06-08 11:07:58 -0400

    added lenght to query, changed case of description

--- main.R
+++ main.R
@@ -8,17 +8,29 @@
 library(biomaRt)
 library(dplyr)
 library(jsonlite)
+library(tools)
 
 # querying the data
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl")
-all.transcripts <- getBM(attributes=c('ensembl_gene_id','ensembl_peptide_id','external_transcript_name',
-                                      'description', 'family_description'), mart = ensembl)
+all.transcripts <- getBM(attributes=c('ensembl_peptide_id', 'external_transcript_name', 'transcript_length',
+                                      'family_description'), mart = ensembl)
 
 # getting only entries with a valid peptide id
-all.peptides <- all.transcripts %>% dplyr::select(-ensembl_gene_id, -description) %>% dplyr::filter(ensembl_peptide_id != '')
+all.peptides <- all.transcripts %>% dplyr::filter(ensembl_peptide_id != '')
 
 # changing header names for json creation
-colnames(all.peptides) <- c('protein_id', 'protein', 'family_description')
+colnames(all.peptides) <- c('protein_id', 'protein', 'trans_length', 'family_description')
+
+# Mixed Case styling for description
+.simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1, 1)), substring(s, 2),
+        sep = "", collapse = " ")
+}
+
+all.peptides$family_description <- tolower(all.peptides$family_description)
+all.peptides$family_description <- toTitleCase(all.peptides$family_description)
+
 
 json <- toJSON(all.peptides)
 cat(json)
